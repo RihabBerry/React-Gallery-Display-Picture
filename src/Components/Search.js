9a64f9d825306900bdc740173feb4876bab56b01
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-07-29 01:25:01 +0200

    Changged Search Component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -10,8 +10,6 @@ class Search extends Component
     }
     mySubmitHandler = (event) =>
     {
-        console.log(event.target.query.value)
-        console.log("this is the query 1", event.target.query.value)
         event.preventDefault();
         this.props.performSearch(event.target.query.value);
     }
@@ -26,18 +24,21 @@ class Search extends Component
 
         return (
             <div >
-                <form onSubmit={this.mySubmitHandler}>
-                    <input
-                        name='query'
-                        type='text'
-                        onChange={this.myChangeHandler}
-                    />
-                    <input
-                        type='submit'
-                    />
-                </form>
-            </div>
 
+                <form className="search-form" onSubmit={this.mySubmitHandler}>
+                    <input type="search"
+                        name="query"
+                        placeholder="Search"
+                        required />
+                    <button type="submit" className="search-button">
+                        <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+                            <path d="M0 0h24v24H0z" fill="none" />
+                        </svg>
+                    </button>
+                </form>
+
+            </div>
         );
 
     };
