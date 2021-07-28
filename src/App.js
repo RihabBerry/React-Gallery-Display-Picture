10f464d2a5962e3ff4f9b3e1ddf8e69ef765d08e
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-07-29 01:25:45 +0200

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import Container from './Components/Container/Container';
+import
+{
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
 function App()
 {
   return (
-    <div className="App">
-      <Container />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Switch>
+          <Route exact path='/' render={() => <Redirect to={"/search"} />} />
+          <Route exact path="/search" component={Container} />
+          <Route path="/search/:query" component={Container} />
+          <Route path="/cats" render={() => { return <Container query={'kittens'} /> }} />
+          <Route path="/dogs" render={() => { return <Container query={'puppys'} /> }} />
+          <Route path="/birds" render={() => { return <Container query={'birds'} /> }} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
